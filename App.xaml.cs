38ad485a349c5760a0cca695833f0e45de55a616
Josh Johnson <sicjoshsic@gmail.com>
2016-02-22 20:19:13 +0000

    Performance Tweak
    Added Profile Optimization code to improve startup speed

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Runtime;
+using System.IO;
+using System.Reflection;
 
 namespace BertUI
 {
@@ -13,5 +16,15 @@ namespace BertUI
     /// </summary>
     public partial class App : Application
     {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            ProfileOptimization.SetProfileRoot(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            ProfileOptimization.StartProfile("Startup.Profile");
+
+            var application = new App();
+            application.InitializeComponent();
+            application.Run();
+        }
     }
 }
